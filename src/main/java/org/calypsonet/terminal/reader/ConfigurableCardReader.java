40d95b38163ce5a2e8ec9bb6d876e913833163b3
LudovicTeixeiraCosta <42935327+LudovicTeixeiraCosta@users.noreply.github.com>
2021-08-27 19:34:41 +0200

    Update ConfigurableCardReader.java

--- src/main/java/org/calypsonet/terminal/reader/ConfigurableCardReader.java
+++ src/main/java/org/calypsonet/terminal/reader/ConfigurableCardReader.java
@@ -12,47 +12,49 @@
 package org.calypsonet.terminal.reader;
 
 /**
- * Configurable Card reader driving the underlying hardware to manage the card detection.
+ * Interface that provides the methods related to the configuration of communication protocols 
+ * for a specific Card Reader instance
  *
  * @since 1.0.0
  */
 public interface ConfigurableCardReader extends CardReader {
 
   /**
-   * Activates the reader protocol having the provided reader protocol name and associates it with
-   * the protocol name defined by the application.
+   * Activates the reader communication protocol by associating the provided reader communication 
+   * protocol name and the protocol name defined by the application.
    *
    * <ul>
-   *   <li>Activates the detection of cards using the provided reader protocol.
-   *   <li>Asks the reader to accept any card using this protocol during the selection phase.
+   *   <li>Activates the detection of cards using the provided reader communication protocol.
+   *   <li>Asks the reader to accept any card using this communication protocol during the selection 
+   *       phase.
    *   <li>Internally associates the two strings provided as arguments.
    * </ul>
    *
-   * <p>The association between the protocol name known by the reader and the protocol name known by
-   * the application is intended to allow a unique protocol name to be set when constructing a card
-   * selector as defined by the <b>Terminal Card API</b> regardless of the type of reader that will
-   * be used.
+   * <p>The association between the communication protocol name known by the reader and the 
+   * communication protocol name known by the application is intended to allow a unique protocol 
+   * name to be set when constructing a card selector as defined by the <b>Terminal Card API</b>
+   * regardless of the type of reader that will be used.
    *
-   * @param readerProtocol The name of the protocol as known by the reader.
-   * @param cardProtocol The name of the protocol as known by the application.
-   * @throws IllegalArgumentException If one of the provided protocol is null or empty.
-   * @throws ReaderProtocolNotSupportedException If the protocol is not supported.
+   * @param readerProtocol The name of the communication protocol as known by the reader.
+   * @param cardProtocol The name of the communication protocol as known by the application.
+   * @throws IllegalArgumentException If one of the provided communication protocol is null or empty.
+   * @throws ReaderProtocolNotSupportedException If the communication protocol is not supported.
    * @since 1.0.0
    */
   void activateProtocol(String readerProtocol, String cardProtocol);
 
   /**
-   * Deactivates the provided card protocol.
+   * Deactivates the provided card communication protocol.
    *
    * <ul>
-   *   <li>Inhibits the detection of cards using this protocol.
-   *   <li>Ask the reader to ignore this protocol if a card using this protocol is identified during
-   *       the selection phase.
+   *   <li>Inhibits the detection of cards using this communication protocol.
+   *   <li>Ask the reader to ignore this protocol if a card using this communication 
+   *       protocol is identified during the selection phase.
    * </ul>
    *
-   * @param readerProtocol The name of the protocol as known by the reader.
-   * @throws IllegalArgumentException If the provided protocol is null or empty.
-   * @throws ReaderProtocolNotSupportedException If the protocol is not supported.
+   * @param readerProtocol The name of the communication protocol as known by the reader.
+   * @throws IllegalArgumentException If the provided communication protocol is null or empty.
+   * @throws ReaderProtocolNotSupportedException If the communication protocol is not supported.
    * @since 1.0.0
    */
   void deactivateProtocol(String readerProtocol);
